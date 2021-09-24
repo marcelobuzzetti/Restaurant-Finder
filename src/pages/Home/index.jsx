83273e9252cce263d103ac09ad6b7c9cd63704df
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2021-09-24 10:59:33 -0300

    material carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Slider from "react-slick";
+import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
-import { Container } from './styles';
+import logo from '../../assets/logo.svg'
+import restaurante from '../../assets/restaurante-fake.png'
+import { Container, Search, Logo, Wrapper, Map, CarouselTitle, Carousel } from './styles';
+import { Card } from '../../components'
 
-const Home = () => <Container>Hello World</Container>;
+const Home = () => {
+    const [inputValue, setInputValue] = useState('');
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true
+    };
+
+    return (
+    <Wrapper>
+        <Container>
+            <Search>
+                <Logo src={logo} alt="Logo do Restaurante" />
+                <TextField
+                    label='Pesquisar'
+                    outlined
+                    trailingIcon={<MaterialIcon role="button" icon="search"/>}
+                    ><Input
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)} />
+                </TextField>
+                <CarouselTitle>Na sua Área</CarouselTitle>
+                <Carousel {...settings}>
+                    <Card photo={restaurante} title="Teste"/>
+                    <Card photo={restaurante} title="Teste"/>
+                    <Card photo={restaurante} title="Teste"/>
+                    <Card photo={restaurante} title="Teste"/>
+                    <Card photo={restaurante} title="Teste"/>
+                </Carousel>
+            </Search>
+        </Container>
+        <Map />
+    </Wrapper>
+    )
+};
 
 export default Home;
