d254a6f8643f93d749f5b0745d4ec82fdd1c6bc2
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2021-09-26 10:50:42 -0300

    Skeleton

--- src/components/RestauranteCard/index.jsx
+++ src/components/RestauranteCard/index.jsx
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactStars from "react-rating-stars-component";
+import { Skeleton } from "..";
 
 import restaurante from '../../assets/restaurante-fake.png'
 
 import { Restaurant, RestaurantInfo, Title, Address, RestaurantPhoto } from './styles'
 
-const RestaurantCard = ({ restaurant, onClick }) => (
-    <Restaurant onClick={onClick}>
-        <RestaurantInfo>
-            <Title>{restaurant.name}</Title>
-            <ReactStars
-                count={5} 
-                isHalf={true}
-                value={restaurant.rating}
-                edit={false}
-                activeColor="#e7711c"
+const RestaurantCard = ({ restaurant, onClick }) => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+    return (
+        <Restaurant onClick={onClick}>
+            <RestaurantInfo>
+                <Title>{restaurant.name}</Title>
+                <ReactStars
+                    count={5} 
+                    isHalf={true}
+                    value={restaurant.rating}
+                    edit={false}
+                    activeColor="#e7711c"
+                />
+                <Address>{restaurant.vicinity || restaurant.formatted_address}</Address>
+            </RestaurantInfo>
+            <RestaurantPhoto
+            imageLoaded={imageLoaded} 
+            src={restaurant.photos ? restaurant.photos[0].getUrl : restaurante} 
+            alt="Foto do Restaurante"
+            onLoad={() => setImageLoaded(true)}
             />
-            <Address>{restaurant.vicinity || restaurant.formatted_address}</Address>
-        </RestaurantInfo>
-        <RestaurantPhoto src={restaurant.photos ? restaurant.photos[0].getUrl : restaurante} alt="Foto do Restaurante"/>
-    </Restaurant>
-)
+            {!imageLoaded && <Skeleton width='100px' heigth='100px' />}
+        </Restaurant>
+    )
+};
 
 export default RestaurantCard;
